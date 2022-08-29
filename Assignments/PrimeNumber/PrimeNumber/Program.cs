ee60f5143f13b543a0aab7bc175e5d9d8e79eab4
KulkarniCKaustubh <111495577+KulkarniCKaustubh@users.noreply.github.com>
2022-08-29 14:11:20 +0530

    Add files via upload

---
+++ Assignments/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+  	        //Declaration  
+                 bool isPrime = true;
+                    int i, j;
+                      //Calculate and display the Prime number  
+              Console.WriteLine("Prime Numbers are : ");
+                  for (i = 2; i <= 100; i++)
+                {
+                       for (j = 2; j <= 100; j++)
+                    {
+                                if (i != j && i % j == 0)
+                        {
+                            isPrime = false;
+                                  break;
+                                       }
+                        }
+                      if (isPrime)
+                    {
+                        Console.WriteLine("\t" + i);
+                            }
+                    isPrime = true;
+                     }
+                Console.ReadLine();
+
+            }
+        }
+}
