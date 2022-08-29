88b4cc4d53a8c46f39106eb60ba5bae8df306b19
KulkarniCKaustubh <111495577+KulkarniCKaustubh@users.noreply.github.com>
2022-08-29 14:13:15 +0530

    Add files via upload

---
+++ Assignments/ShoppingLib/ShoppingLib/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLib
+{
+    public class Product
+    {
+         private string productname;
+        public string ProductName
+            {
+                get { return productname; }
+                set { productname = value; }
+
+            }
+
+            private double price;
+        public double Price 
+        { 
+            get { return price; }
+            set { price = value; } 
+        }
+
+            private int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public double GetProductCost()
+        {
+           double Cost = Quantity * Price;
+            return Cost;
+        }
+
+        public Product( string productName, double price, int quantity)
+        {
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"ProductName {ProductName}, Price {Price} Quantity {Quantity} Cost {GetProductCost()}");
+        }
+
+    }
+    }
---
+++ Assignments/ShoppingLib/TestShopping/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingLib;
+
+namespace TestShopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("ABC", 100, 10);
+           
+            double cost = product.GetProductCost();
+
+            Console.WriteLine("Total Cost of Product = " + cost);
+            Console.WriteLine(product);
+
+            Product p1 = new Product("XYZ", 100, 20);
+
+            double Cost = p1.GetProductCost();
+            Console.WriteLine(p1);
+            Product p2 = new Product("MNO", 200, 25);
+   
+            double Cost2 = p2.GetProductCost();
+            Console.WriteLine(p2); 
+            Console.ReadLine();
+        }
+    }
+}
