3eadbac263d97be01b2dc8b07f08ad0d671bdb8f
KulkarniCKaustubh <111495577+KulkarniCKaustubh@users.noreply.github.com>
2022-08-29 14:10:30 +0530

    Add files via upload

---
+++ Assignments/FibonaccySeries/FibonaccySeries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonaccySeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+
+
+            int y = 1;
+
+
+
+            int z = 1;
+
+            while (x <= 100)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+        }
+    }
+
