d61404c49a1fa1ca9763f4143143ca7de3969dd1
KulkarniCKaustubh <111495577+KulkarniCKaustubh@users.noreply.github.com>
2022-08-29 14:14:32 +0530

    Add files via upload

---
+++ Assignments/PersonalInfoLib/PersonDataTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalInfoLib;
+
+namespace PersonDataTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Kaustubh", "Kulkarni", "KK@email.com", Convert.ToDateTime("24/12/1999"));
+            Console.WriteLine(p1);
+
+            Console.WriteLine("Is Person is Adult " + p1.IsAdult());
+
+            Console.ReadLine();
+        }
+    }
+}
