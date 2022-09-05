3b90592ad4fce54a5e008b63aeb1da8830936f1a
KulkarniCKaustubh <111495577+KulkarniCKaustubh@users.noreply.github.com>
2022-09-05 11:02:39 +0530

    Add files via upload

---
+++ HRLib/HRAppl/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLib;
+
+namespace HRAppl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice, num;
+
+            List<ConfirmEmployee> empList = new List<ConfirmEmployee>();
+            empList.Add(new ConfirmEmployee { Name = "KK", Address = "DN DR Akola", TypeEmployee = "Trainee", Basic = 21000, Designation = "Sofware Developer" });
+            empList.Add(new ConfirmEmployee { Name = "SK", Address = "Pune", TypeEmployee = "Developer", Basic = 25000, Designation = "Tester" });
+
+
+
+            List<Trainee> trainee = new List<Trainee>();
+            trainee.Add(new Trainee { Name = "Kaustubh", Address = "Akola", TypeEmployee = "Trainee" });
+            trainee.Add(new Trainee { Name = "Shri", Address = "Pune", TypeEmployee = "Trainee" });
+
+            do
+            {
+                int number2 = 0;
+                Console.WriteLine("1. Show All Employees");
+                Console.WriteLine("2. Show Employee which you want");
+                Console.WriteLine();
+                Console.WriteLine("Enter a number from above choice");
+                int result = int.Parse(Console.ReadLine());
+
+                if (result == 1)
+                {
+                    foreach (ConfirmEmployee empno in empList)
+                    {
+                        Console.WriteLine(empno);
+                        Console.WriteLine(empList);
+                    }
+                    foreach (Trainee empno in trainee)
+                    {
+                        Console.WriteLine(empno);
+                        Console.WriteLine(trainee);
+                    }
+                }
+                else if (result == 2)
+                {
+                    Console.WriteLine("Enter a Employee Number which you want : ");
+                    num = Console.ReadLine();
+
+                    foreach (ConfirmEmployee empno in empList)
+                    {
+                        if (empno.EmpNo == num)
+                        {
+                            Console.WriteLine(empno);
+                            number2++;
+                            break;
+                        }
+                    }
+
+                    foreach (Trainee empno1 in trainee)
+                    {
+                        if (empno1.EmpNo == num)
+                        {
+                            Console.WriteLine(empno1);
+                            number2++;
+                            break;
+                        }
+                    }
+
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("Employe Not Found");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you Want to Continue Press 'y' or 'n' :");
+                choice = Console.ReadLine();
+
+            } while (choice == "y");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HRLib/Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLib;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfirmEmployee confirmEmployee = new ConfirmEmployee("kk", "akola",25000, "trainee");
+            Console.WriteLine(confirmEmployee);
+
+            ConfirmEmployee confirmEmployee1 = new ConfirmEmployee();
+            Console.WriteLine(confirmEmployee1);
+
+            Trainee trainee = new Trainee("kk","akola",15, 1500);
+            Console.WriteLine(trainee);
+
+            Console.WriteLine(trainee.CalculateSalary());
+
+            Console.WriteLine(confirmEmployee1.CalculateSalary());
+
+            Console.ReadLine();
+
+        }
+    }
+}
